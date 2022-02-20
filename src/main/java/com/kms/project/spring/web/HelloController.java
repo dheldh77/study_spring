aceb87c8d1c9fec57ee902466d4181ba1f8e719f
dhelhd77 <dheldh77@icloud.com>
2022-02-20 23:38:36 +0900

    2. 테스트 코드 작성

---
+++ src/main/java/com/kms/project/spring/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.kms.project.spring.web;
+
+import com.kms.project.spring.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    // @RequestParam
+    // 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloResponseDto(@RequestParam("name") String name,
+                                             @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+}
