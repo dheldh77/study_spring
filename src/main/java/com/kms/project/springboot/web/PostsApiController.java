0a5654016f0cd026a651e505e91ae69b89798b0b
dhelhd77 <dheldh77@icloud.com>
2022-03-02 06:50:41 +0900

    3. Create, Update 추가

---
+++ src/main/java/com/kms/project/springboot/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.kms.project.springboot.web;
+
+import com.kms.project.springboot.service.posts.PostsSerivce;
+import com.kms.project.springboot.web.dto.PostsResponseDto;
+import com.kms.project.springboot.web.dto.PostsSaveRequestDto;
+import com.kms.project.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsSerivce postsSerivce;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsSerivce.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsSerivce.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsSerivce.findById(id);
+    }
+}
---
+++ src/test/java/com/kms/project/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,29 @@
+package com.kms.project.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void lombok_function_test() {
+        // given
+        String name = "test";
+        int amount = 100;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+        // assertThat
+        // assertj라는 테스트 검증 라이브러리의 검증 메소드
+        // 검증하고 싶은 대상을 메소드 인자로 받음
+        // 메소드 체이닝이 지원되어 isEqualTo와 같이 메소드를 이어서 사용 가능
+
+        // isEqualTo
+        // assertj의 동등 비교 메소드
+        // assertThat에 있는 값과 isEqualTo의 값을 비교해서 같을 때만 성공
+    }
+}
