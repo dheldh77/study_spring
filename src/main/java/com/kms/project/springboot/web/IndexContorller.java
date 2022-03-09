dc69fe9f2b29f09c611a87e3fd611ca5751e5ecc
dhelhd77 <dheldh77@icloud.com>
2022-03-10 00:48:48 +0900

    mustache crud

--- src/main/java/com/kms/project/springboot/web/IndexContorller.java
+++ src/main/java/com/kms/project/springboot/web/IndexContorller.java
@@ -1,10 +1,12 @@
 package com.kms.project.springboot.web;
 
 import com.kms.project.springboot.service.posts.PostsSerivce;
+import com.kms.project.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,5 +22,13 @@ public class IndexContorller {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsSerivce.findById(id);
+        model.addAttribute("posts", dto);
+
+        return "posts-update";
     }
 }
--- src/main/java/com/kms/project/springboot/web/PostsApiController.java
+++ src/main/java/com/kms/project/springboot/web/PostsApiController.java
@@ -27,4 +27,10 @@ public class PostsApiController {
     public PostsResponseDto findById (@PathVariable Long id) {
         return postsSerivce.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsSerivce.delete(id);
+        return id;
+    }
 }
