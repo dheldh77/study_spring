cbd0e702b800e2c299e344ed0396eb675d55e7f6
dhelhd77 <dheldh77@icloud.com>
2022-03-13 20:08:54 +0900

    google login

--- src/main/java/com/kms/project/springboot/web/IndexContorller.java
+++ src/main/java/com/kms/project/springboot/web/IndexContorller.java
@@ -1,5 +1,7 @@
 package com.kms.project.springboot.web;
 
+import com.kms.project.springboot.config.auth.LoginUser;
+import com.kms.project.springboot.config.auth.dto.SessionUser;
 import com.kms.project.springboot.service.posts.PostsSerivce;
 import com.kms.project.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +10,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexContorller {
     private final PostsSerivce postsSerivce;
+    private final HttpSession httpSession;
 
+    // 1. @LoginUser SessionUser user
+    // - 기존에 (User) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선
+    // - 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 되었음
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsSerivce.findAllDesc());
+        // 1. (SessionUser) httpSession.getAttribute("user")
+        // - 앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+        // - 즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // 2. if(user != null)
+        // - 세션에 저장된 값이 있을 때만 model에 userName으로 등록
+        // - 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 됨
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -31,4 +49,5 @@ public class IndexContorller {
 
         return "posts-update";
     }
+
 }
