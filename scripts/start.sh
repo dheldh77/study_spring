c22eb4492a071cdc910817c22fca194888cfea81
dheldh77 <35682251+dheldh77@users.noreply.github.com>
2022-04-17 14:39:13 +0900

    [Test] 무중단 배포 테스트

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,14 +5,14 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=springboot2-webservice
+PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 어플리케이션 배포"
+echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
